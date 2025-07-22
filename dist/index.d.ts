69f9687e1a2b783a59aa76c045d25796663f2b75
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-07-21 21:03:24 -0400

    cleaned

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import BrainKBAssistant from './components/BrainKBAssistant';
-export default BrainKBAssistant;
-export { default as BrainKBAssistantWrapper } from './components/BrainKBAssistantWrapper';
-export type { BrainKBConfig } from './components/BrainKBAssistantWrapper';
-//# sourceMappingURL=index.d.ts.map
